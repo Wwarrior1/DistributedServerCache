32f15706e6061ab99fab134928956d3e49f262a9
Maciej Kocot <mkocot@op.pl>
2017-04-06 20:29:42 +0200

    comments

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -4,30 +4,20 @@ from utils.solution_checker import calculate_score
 
 class Algorithm:
 
-    def __init__(self, n, m, e, nep, nsp, ngh, max_iter):
-        self.n = n
-        self.m = m
-        self.e = e
-        self.nep = nep
-        self.nsp = nsp
-        self.ngh = ngh
-        self.max_iter = max_iter
-
     @staticmethod
     def execute(data):
 
         """
-        :param data:
         :return: score of the best solution found
         """
 
-        n = 55
-        m = 7
-        e = 2
-        nep = 44
-        nsp = 27
+        n = 5
+        m = 3
+        e = 1
+        nep = 4
+        nsp = 2
         ngh = 5
-        max_iter = 122
+        max_iter = 2
 
         pool = [AlgorithmUtils.random_solution(data) for _ in range(n)]
 
@@ -38,8 +28,6 @@ class Algorithm:
             elite_solutions = solutions_ranking[0:e]
             good_solutions = solutions_ranking[e:m]
 
-            # AlgorithmUtils.print_solution_pool(solutions_ranking)
-
             pool = []
             for es in elite_solutions:
                 pool.append(AlgorithmUtils.best_in_neighbourhood(data, es, ngh, nep))
@@ -48,5 +36,5 @@ class Algorithm:
             for _ in range(n-m):
                 pool.append(AlgorithmUtils.random_solution(data))
 
-        print(pool[0])
+        print(max(pool, key=lambda s: calculate_score(data, s)))
         return calculate_score(data, pool[0])
--- algorithm/utils.py
+++ algorithm/utils.py
@@ -26,50 +26,73 @@ class AlgorithmUtils:
         return res
 
     @staticmethod
-    def print_solution_pool(pool):
-        print("\nPOOL:\n", "\n".join(map(str, pool)))
-
-    @staticmethod
     def random_neighbour(data, solution, radius):
+
+        """
+        Na razie to dziala tak, ze 'radius' razy losujemy sasiada w odleglosci 1
+        Potencjalnie mozemy wyladowac w odleglosci radius lub mniejszej (w szczegolnosci wrocic do solution) - to chyba
+            nie szkodzi?
+        Nie ma tez gwarancji, ze sie nie powtorza - to chyba nie szkodzi?
+        Prawdopodobienstwo wylosowania kazdego rozwiazania niekoniecznie jest rowne. Ale nie wiem jak inaczej to zrobic.
+
+        TODO or not TODO? Zostawiamy to, czy ktos ma lepszy pomysl? Ja nie. :)
+        """
 
         res = AlgorithmUtils.solution_copy(solution)
         if radius == 0:
             return res
 
-        acceptable_ops = [(s, f)
+        acceptable_ops = [(s, v)
                           for s in range(data.amount_of_cache_servers)
-                          for f in range(len(data.videos_sizes))
-                          if f in res[s] or data.videos_sizes[f] <= AlgorithmUtils.free_space(data, s, solution)]
+                          for v in range(len(data.videos_sizes))
+                          if v in res[s] or data.videos_sizes[v] <= AlgorithmUtils.free_space(data, solution, s)]
 
         op = random.choice(acceptable_ops)
         s = op[0]
-        f = op[1]
+        v = op[1]
 
-        if f in res[s]:
-            res[s].remove(f)
+        if v in res[s]:
+            res[s].remove(v)
         else:
-            res[s].append(f)
+            res[s].append(v)
 
         return AlgorithmUtils.random_neighbour(data, res, radius - 1)
 
     @staticmethod
     def best_in_neighbourhood(data, solution, radius, number_of_bees):
+
+        """
+        Wysylamy losowo 'number_of_bees' pszczol do zbadania obszaru o promieniu radius.
+        Zwracamy najlepsze z tych rozwiazan.
+
+        TODO Staralem sie wyliczac score od razu, zeby potem nie robic tego ponownie przy max(...) i w ostatnim ifie
+        Stad taka dziwna lista krotek: neighbours = [(solution, score(solution)), ...]
+        Da sie to bardziej zoptymalizowac?
+        """
+
         neighbours = []
         for _ in range(number_of_bees):
             neighbour = AlgorithmUtils.random_neighbour(data, solution, radius)
             neighbours.append((neighbour, calculate_score(data, neighbour)))
-        best_neighbour = max(neighbours, key=lambda s: calculate_score(data, s[0]))
+        best_neighbour = max(neighbours, key=lambda s: s[1])
         return best_neighbour[0] if best_neighbour[1] > calculate_score(data, solution) else solution
 
     @staticmethod
-    def free_space(data, server, solution):
-        res = data.cache_size
-        for v in solution[server]:
-            res -= data.videos_sizes[v]
-        return res
+    def free_space(data, solution, server):
+
+        """
+        Ile wolnych MB na danym serwerze (do sprawdzania, czy mozna dodac film)
+        """
+
+        return data.cache_size - sum([data.videos_sizes[v] for v in solution[server]])
 
     @staticmethod
     def solution_copy(solution):
+
+        """
+        Bo python.
+        """
+
         res = dict()
         for s in solution:
             res[s] = solution[s][:]
